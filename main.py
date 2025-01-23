2032a278b7f3b9509aab2b18168952a1fba82c81
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2025-01-22 23:27:26 -0500

    adjust for preflight

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from routers.dashboard.tenant.ticket_route import ticket_router
 from routers.dashboard.owner.update_tickets import mngm_ticket_router
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://helixpropertymanagement-718e761927a1.herokuapp.com"],
+    allow_origins=[
+        "http://localhost:3000",  # Local development frontend
+        "https://helixpropertymanagement-718e761927a1.herokuapp.com"  # Deployed frontend
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],  # Allow necessary methods
+    allow_headers=["Content-Type", "Authorization", "X-Custom-Header"],  # Allow required headers
 )
 
 app.include_router(router)
